bd290795247dd0a7b023039cf81a7ded69db1df3
ZeroMan2002 <toborobotic@gmail.com>
2023-12-04 02:31:34 +0000

    Public

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // JavaScript to update release dates or other interactive elements
+    // Example dynamic content loading (implement according to your backend/API)
+    fetch('api/apps.json')
+        .then(response => response.json())
+        .then(apps => {
+            apps.forEach(app => {
+                const appElement = document.createElement('article');
+                appElement.className = 'app';
+                appElement.innerHTML = `
+                    <a href="${app.link}">
+                        <img src="${app.image}" alt="${app.name}">
+                        <span class="app-info">${app.description}. Released: ${app.releaseDate}.</span>
+                    </a>
+                `;
+                document.querySelector('.app-container').appendChild(appElement);
+            });
+        })
+        .catch(error => console.error('Error loading app data:', error));
 });
